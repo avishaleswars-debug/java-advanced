28a2631ba4a7c003c4459941410af1be85914de4
avishaleswars-debug <avishaleswars@gmail.com>
2025-12-23 18:44:47 +0530

    hashed

--- StudentManagementSystemJDBC/DBConnection.java
+++ StudentManagementSystemJDBC/DBConnection.java
@@ -6,7 +6,7 @@ public class DBConnection {
     public static Connection getConnection()throws Exception{
           String url="jdbc:mysql://localhost:3306/studDB";
         String name="root";
-        String pass="ajay@12345";
+        String pass="********";
    
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con=DriverManager.getConnection(url, name, pass);

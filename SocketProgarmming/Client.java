6ee25239ef19a5a44f05d2de0d441bd9ceacdd3d
avishaleswars-debug <avishaleswars@gmail.com>
2025-12-25 15:05:43 +0530

    simple cat app between client and server

--- SocketProgarmming/Client.java
+++ SocketProgarmming/Client.java
@@ -2,13 +2,29 @@ package SocketProgarmming;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args)throws IOException {
         Socket s=new Socket("localhost",9999);
-        DataInputStream io=new DataInputStream(s.getInputStream());
-       String line= io.readUTF();
-       System.out.println(line);
-       s.close();
+          DataOutputStream ds=new DataOutputStream(s.getOutputStream());
+                DataInputStream io=new DataInputStream(s.getInputStream());
+                Scanner sc=new Scanner(System.in);
+               new Thread(()->{try {while (true) {
+                    
+String li=io.readUTF();
+System.out.println("Server  "+li);
+               }
+            }
+               catch(Exception e){
+                System.out.println(e);
+               }
+    }).start();
+                while (true) {
+  String line=sc.nextLine();
+                    ds.writeUTF(line);
+            
+                }
+              
     }
 }
--- SocketProgarmming/Server.java
+++ SocketProgarmming/Server.java
@@ -1,15 +1,30 @@
 package SocketProgarmming;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.*;
-
+import java.util.*;
 public class Server {
     public static void main(String[] args) throws Exception {
         ServerSocket ss=new ServerSocket(9999);
         Socket s=ss.accept();
         DataOutputStream ds=new DataOutputStream(s.getOutputStream());
-ds.writeUTF("heollo");
-ds.flush();
-        ss.close();
+                DataInputStream io=new DataInputStream(s.getInputStream());
+                Scanner sc=new Scanner(System.in);
+                new Thread(()->{try {while (true) {
+                    
+String li=io.readUTF();
+System.out.println("Client  "+li);
+               }
+            }
+               catch(Exception e){
+                System.out.println(e);
+               }
+    }).start();
+                while (true) {
+  String line=sc.nextLine();
+                    ds.writeUTF(line);
+        
+                }
     }
 }

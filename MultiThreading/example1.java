b55d940c54f614983c8a97b8ffdccb5381ddbf66
avishaleswars-debug <avishaleswars@gmail.com>
2026-01-29 21:53:51 +0530

    runnable interface

--- MultiThreading/example1.java
+++ MultiThreading/example1.java
@@ -5,7 +5,7 @@ public void run(){
 System.out.println("hello world");
 }
 }
-public class example1 extends Thread{
+public class example1 implements Runnable{
     public void run(){
         for(int i=0;i<6;i++)
         System.out.println("hello");
@@ -13,8 +13,10 @@ public class example1 extends Thread{
     public static void main(String[] args) {
         ex e=new ex();
         example1 ee=new example1();
+        Thread t=new Thread(ee);
+        t.setPriority(10);
         e.start();
-        ee.start();
+        t.start();
     }
     
 }
